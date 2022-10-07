0b6c030fdf2f3426ccdeabe635a4a3ddf08320f0
Dorian Loret <dorian.loret20@gmail.com>
2022-10-07 16:47:34 +0200

    end

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -1,5 +1,22 @@
+import { ADD_USER_LIKE } from "../actions/post.action";
+import { GET_USER } from "../actions/user.action";
+
 const initialState = {};
 
 export default function userReducer(state = initialState, action) {
-  return initialState;
+  switch (action.type) {
+    case GET_USER:
+      return action.payload;
+    case ADD_USER_LIKE:
+      return state.map((user) => {
+        if (user.id === action.payload.id) {
+          return {
+            ...user,
+            likes: action.payload.likes,
+          };
+        } else return user;
+      });
+    default:
+      return state;
+  }
 }
